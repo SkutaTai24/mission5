bb37b0087d4a47d7090e9e47b83cca69a4bbca04
SkutaTai24 <jaden@missionreadyhq.com>
2022-03-11 07:54:08 +1300

    Test to run deploy to gh pages

--- src/Pages/CarInsurance.js
+++ src/Pages/CarInsurance.js
@@ -9,6 +9,7 @@ function CarInsurance () {
       <img className='Car1' src={Car1} alt='Car1' />
       <h2>We have two types of Insurance Cover. See details below!</h2>
       <img className='MVImg' src={MVImg} alt='MVImg' />
+      <h1>Test1</h1>
 
     </div>
   )
